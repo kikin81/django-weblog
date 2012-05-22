ed191b6e776fb3835bd61c038111222e16faec40
Francisco S Velazquez <kikin81@gmail.com>
2012-05-21 20:51:27 -0700

    added mingus to the settings file

--- weblog/settings.py
+++ weblog/settings.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'mingus',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
