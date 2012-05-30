fbf265dc2642e5138c6e9eb1c3280776f10dc252
Francisco S Velazquez <kikin81@gmail.com>
2012-05-30 12:57:23 -0700

    working registration

--- my_auth/activation.py
+++ my_auth/activation.py
@@ -1,12 +1,13 @@
 from django.core.mail import send_mail
 from hashlib import md5
+from django.contrib.auth.models import User
 from django.template import loader, Context
 import time
 
 def send_activation(user):
     time.sleep(10)
     code = md5(user.username).hexdigest()
-    url = "http://127.0.0.1:8000/activate/?user=%s&code=%s" % (user.username,  code)
+    url = "http://127.0.0.1:8000/accounts/activate/?user=%s&code=%s" % (user.username,  code)
     print(url)
     template = loader.get_template('registration/activation.html')
     context = Context({
@@ -15,3 +16,12 @@ def send_activation(user):
     })
  
     send_mail('Activate account at super site', template.render(context), 'no-reply@example.com', [user.email])
+
+def activate_user(username, code):
+    if code == md5(username).hexdigest():
+        user = User.objects.get(username=username)
+        user.is_active = True
+        user.save()
+        return True
+    else:
+        return False
--- my_auth/views.py
+++ my_auth/views.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import User
 from django.core.context_processors import csrf
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render_to_response
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
+
+from activation import activate_user
+from hashlib import md5
 from forms import RegisterForm
 
 @csrf_exempt
@@ -23,16 +26,7 @@ def activate(request):
     user = request.GET.get('user')
     code = request.GET.get('code')
 
-    if active_user(user, code):
+    if activate_user(user, code):
         return HttpResponseRedirect("/")
     else:
         raise Http404
-
-def activate_user(username, code):
-    if code == md5(username).hexdigest():
-        user = User.objects.get(username=username)
-        user.is_active = True
-        user.save()
-        return True
-    else:
-        return False
