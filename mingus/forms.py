1884068a986b22bb30fa33226ee73f4c7e774145
Francisco S Velazquez <kikin81@gmail.com>
2012-06-01 10:50:19 -0700

    added a date picker to the pub date

--- mingus/forms.py
+++ mingus/forms.py
@@ -1,8 +1,17 @@
-from mingus.models import Entry
+from mingus.models import Entry, Tag
 from django.forms import ModelForm
 from datetime import date
 
 class EntryForm(ModelForm):
+    # how to handle tags
+    # text entry with comma separated values?
+    # click a + and a pop up comes with the tag form
+    # if the request is successful, add it to the "list"
     class Meta:
         model = Entry
         exclude = ('author', 'slug', 'tags')
+
+class TagForm(ModelForm):
+
+    class Meta:
+        model = Tag
